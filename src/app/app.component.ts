c099b6e1f8c5418ee118ed3b64a84182fb2711c2
Andrea <a.dilorenzo@dema-engineering.it>
2022-01-28 08:59:45 +0100

    fix guard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,7 +13,7 @@ export class AppComponent implements OnInit {
 
   ngOnInit(): void {
     this.setI18n();
-    console.log('postazione ' + localStorage.getItem('guid'));
+    console.log('postazione ' + localStorage.getItem('guId'));
   }
 
   private setI18n(): void {
--- src/app/core/wsGuard.ts
+++ src/app/core/wsGuard.ts
@@ -1,5 +1,6 @@
 import { Inject, Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
+import { SessionService } from '@npt/npt-template';
 import { WorkstationService } from '../service/workstation.service';
 
 
@@ -10,21 +11,28 @@ export class WorkStationGuard implements CanActivate {
   private guard = true;
   constructor(
     private router: Router,
-    private workStationService: WorkstationService
+    private workStationService: WorkstationService,
+    private sessionService: SessionService
   ) { }
 
   async canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Promise<boolean> {
-    const guid = localStorage.getItem('guid');
-    this.workStationService.getWorkstation(guid).subscribe(
-      data => {
-        if (data) {
-          this.guard = true;
-        }else {
-          this.router.navigate(['workstation-notfound']);
-          this.guard = false;
+    const fingerId = localStorage.getItem('fingerId');
+    const guId = localStorage.getItem('guId');
+    if (guId){
+      this.guard = true;
+    }else{
+      this.workStationService.getWorkstation(fingerId).subscribe(
+        data => {
+          if (data) {
+            this.guard = true;
+            this.sessionService.setSessionLocal('guId', data.id);
+          }else {
+            this.router.navigate(['workstation-notfound']);
+            this.guard = false;
+          }
         }
-      }
-    );
+      );
+    }
     return this.guard;
   }
 }
