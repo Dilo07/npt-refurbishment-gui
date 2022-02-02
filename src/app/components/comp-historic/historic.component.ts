a07a63b7edb953c22e005320e60ad96bad51e44f
Andrea <a.dilorenzo@dema-engineering.it>
2022-02-02 17:47:37 +0100

    espansion panel

--- src/app/components/comp-historic/historic.component.ts
+++ src/app/components/comp-historic/historic.component.ts
@@ -1,3 +1,4 @@
+import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { BatchBoxService } from 'src/app/service/batch-box.service';
 import { MatTableDataSource } from '@angular/material/table';
@@ -12,15 +13,29 @@ import { Batch } from '../domain/domain';
   width: 100%;
   background-color: aliceblue;
   }
+  .mat-column-expandButton { max-width: 2%; }
+  .mat-column-id { max-width: 18%;}
+  .mat-column-lotNumber { max-width: 20%}
+  .mat-column-hardware { max-width: 20%}
+  .mat-column-dateIns { max-width: 20%}
+  .mat-column-dateClose { max-width: 20%}
   `
-  ]
+  ],
+  animations: [
+    trigger('detailExpand', [
+      state('collapsed', style({ height: '0px', minHeight: '0' })),
+      state('expanded', style({ height: '*' })),
+      transition('expanded <=> collapsed', animate('225ms cubic-bezier(0.4, 0.0, 0.2, 1)')),
+    ]),
+  ],
 })
 export class HistoricComponent implements OnInit {
   @ViewChild(MatPaginator) paginator: MatPaginator;
   public panelOpenState = false;
   public complete = true;
+  public expandedElement: any | null;
   public dataSource = new MatTableDataSource<Batch>();
-  public displayedColumns: string[] = ['id', 'lotNumber', 'hardware', 'dateIns', 'dateClose'];
+  public displayedColumns: string[] = ['expandButton', 'id', 'lotNumber', 'hardware', 'dateIns', 'dateClose'];
 
   constructor(
     private batchBoxService: BatchBoxService
