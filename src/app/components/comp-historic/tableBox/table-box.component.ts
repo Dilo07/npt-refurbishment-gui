eab5ca52b7cca92f3d070f63bf69f9d3793a18ec
Andrea <a.dilorenzo@dema-engineering.it>
2022-02-16 10:07:28 +0100

    ready to print true

--- src/app/components/comp-historic/tableBox/table-box.component.ts
+++ src/app/components/comp-historic/tableBox/table-box.component.ts
@@ -2,9 +2,13 @@ import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/cor
 import { MatTableDataSource } from '@angular/material/table';
 import { Subscription } from 'rxjs';
 import { BatchBoxService } from 'src/app/service/batch-box.service';
+import ZebraBrowserPrintWrapper from 'zebra-browser-print-wrapper';
+import { Device } from 'zebra-browser-print-wrapper/lib/types';
 import { Box } from '../../domain/domain';
-import * as BrowserPrint from '../../../../assets/BrowserPrint-3.0.216.min.js';
+/* import * as BrowserPrint from '../../../../assets/BrowserPrint-3.0.216.min.js';
+import * as PrintStatus from '../../../../assets/BrowserPrint-Zebra-1.0.216.min.js';
 declare const BrowserPrint: BrowserPrint;
+declare const PrintStatus: PrintStatus; */
 
 @Component({
   selector: 'app-table-box',
@@ -38,26 +42,31 @@ export class TableBoxComponent implements OnChanges {
   }
 
   public async print(): Promise<void> {
-    BrowserPrint.getDefaultDevice('printer', (device) => {
-      console.log(device);
-    });
-    BrowserPrint.getStatus((status) => { console.log(status); });
-    /* const device: Device = {
-    name: 'zebra',
-    deviceType: 'printer',
-    connection: 'network',
-    uid: '192.168.60.202:9100',
-    provider: 'com.zebra.ds.webdriver.desktop.provider.DefaultDeviceProvider',
-    manufacturer: 'Zebra Technologies',
-    version: 0
-  };
-  const zpl = `^XA ^BY2,2,100 ^FO20,20^BC^FD001^FS ^XZ`;
-  const zebra = new ZebraBrowserPrintWrapper(); */
-    /* const defaulPrinter = await zebra.getDefaultPrinter();
-    console.log(defaulPrinter); */
-    /* zebra.setPrinter(device);
+    /*  let device: any;
+     BrowserPrint.getDefaultDevice('printer', (deviceResp) => {
+       device = deviceResp;
+       console.log(device);
+     });
+     device.sendFile('^XA ^BY2,2,100 ^FO20,20^BC^FD001^FS ^XZ', (data) => {
+       console.log(data)
+     }) */
+    /* PrintStatus.getStatus.then(data => console.log(data)) */
+    const device: Device = {
+      name: 'zebra',
+      deviceType: 'printer',
+      connection: 'network',
+      uid: '192.168.60.202:9100',
+      provider: 'com.zebra.ds.webdriver.desktop.provider.DefaultDeviceProvider',
+      manufacturer: 'Zebra Technologies',
+      version: 0
+    };
+    const zpl = `^XA ^BY2,2,100 ^FO20,20^BC^FD001^FS ^XZ`;
+    const zebra = new ZebraBrowserPrintWrapper();
+    const defaulPrinter = await zebra.getDefaultPrinter();
+    console.log(device);
+    zebra.setPrinter(device);
     const printerStatus = await zebra.checkPrinterStatus();
-    console.log(printerStatus); */
+    console.log(printerStatus);
     /* zebra.print(zpl); */
   }
 }
